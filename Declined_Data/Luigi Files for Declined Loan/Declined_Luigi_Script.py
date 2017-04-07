d97fa6c5f9d7dd57fc13086144c655f1b3675de2
Sweta <bajaj.sw@husky.neu.edu>
2017-04-07 18:00:42 -0400

    Changes in Declined Loan Luigi Script

--- Declined_Data/Luigi Files for Declined Loan/Declined_Luigi_Script.py
+++ Declined_Data/Luigi Files for Declined Loan/Declined_Luigi_Script.py
@@ -1,13 +1,17 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 # Filename: run_luigi.py
 import luigi
 
 import Declined_Validation as validate
 import Declined_Merge_File as merge_file
+import Declined_Loan_S3_Upoad_File as s3
+import time
+
+
 
 class Download_files(luigi.Task):
      
@@ -26,12 +30,37 @@ class Validate_Files(luigi.Task):
      
     def requires(self):
         return [Download_files()]
+    
+    def output(self):
+        return luigi.LocalTarget("ValidatedCombinedRejectLoan.csv")
 
     def run(self):
         print("running validate method..")
         validate.fill_empty_values()
+        
+    
+
+class UploadToS3(luigi.Task):
+    #awsKey = luigi.Parameter(config_path=dict(section='path', names='aws_key'))
+    #awsSecret = luigi.Parameter(config_path=dict(section='path', names='aws_secret'))
+    def requires(self):
+        time.sleep(20)
+        return [Validate_Files()]
+
+    def run(self):
+        print("running validate method..")
+        s3.uploadToS3()
+        
+      
+        
+
     
                 
 if __name__ == '__main__':
-    luigi.run(['Validate_Files','--local-scheduler'])
+    luigi.run(['UploadToS3','--local-scheduler'])
+
+
+# In[ ]:
+
+
 
